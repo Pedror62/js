ca128f5e552d48045ffc9624b0a767b3c7d49a86
pedror62 <pedrohenriquerocha62gmail.com>
2022-08-10 17:17:46 -0300

    java script

---
+++ index.js
@@ -0,0 +1,24 @@
+var vel = 20
+console.log({vel} )
+        if( vel < 10) {
+            console.log('continue com cuidade, use o cinto sempre ')
+    }
+console.log('mais cuidado voce foi multado ')
+
+
+
+
+/*var pais = 'argentina'
+console.log( {pais})
+ if(pais != 'brasil'){
+        console.log('voce e estrangeiro')
+    }  else(
+            console.log('voce e brasileiro')
+        )*/
+
+
+var idade =22
+if(
+    
+)
+consol.log
---
+++ teste.index.js
@@ -0,0 +1,10 @@
+var idade = 66
+ console.log({idade} )
+ if (idade < 22 ){
+    console.log('não  vota')
+ }
+    else if(idade < 22 || idade > 65){
+        console.log('vota se quiser ')
+    } else {
+        console.log('voto obrigatorio')
+    }
